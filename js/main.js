7fe6c5ef447eea96d9927aa3e8791d9797104625
DANY235 <88884453+DANY235@users.noreply.github.com>
2021-12-13 20:08:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     var floorPath =$(".home-image path");// каждый отдельный этаж в SVG
     var counterUp = $(".counter-up");/* Кнопка увеличения этажа */
     var counterDown = $(".counter-down");/* Кнопка уменьшения этажа */
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+
 
     // функция при наведении мышкой на этаж
     floorPath.on('mouseover', function () {
@@ -10,6 +14,13 @@ $(document).ready(function () {
         currentFloor = $(this).attr("data-floor");//получаем значение этажа
         $('.counter').text(currentFloor);//записываем значение в счетчик справа
     });
+
+
+    floorPath.on('click', toggleModal); /**при клике на этаж, вызывает окно */
+    modalCloseButton.on('click', toggleModal); /**клик на кнопку закрыть */
+    viewFlatsButton.on('click', toggleModal);
+
+
     // отслеживаем клик по кнопке вверх
     counterUp.on('click', function () {
         if (currentFloor < 18) { // проверяем значение этажа, оно не должно быть больше 18
@@ -31,5 +42,8 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
-    })
+    });
+    function toggleModal () { //функция открыть/закрыть окно
+        modal.toggleClass('is-open');
+    }
 });
